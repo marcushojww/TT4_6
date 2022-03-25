c1cb719c8de75d8dbf8f55ab130250af19e71c25
Marcus Ho Jw <marcushojww@gmail.com>
2022-03-25 12:36:14 +0800

    Layout implementation

---
+++ client/loan-app/src/components/Layout.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { useNavigate, Outlet, NavLink } from "react-router-dom";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+
+function Layout() {
+  const navigate = useNavigate();
+
+  return (
+    <Box sx={{ display: "flex", flexDirection: "column", width: "100vw" }}>
+      <Box
+        sx={{
+          width: "100%",
+          p: 4,
+          bgcolor: "text.primary",
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <NavLink to={"/home"}>
+          <Typography>Home</Typography>
+        </NavLink>
+        <NavLink to={"/requestLoan"}>Request for loan</NavLink>
+      </Box>
+      <Box>
+        <Outlet />
+      </Box>
+    </Box>
+  );
+}
+
+export default Layout;
