d1dd2885a395fd8fd08b8a79d5019cf572e0b5b8
Marcus Ho Jw <marcushojww@gmail.com>
2022-03-25 16:03:27 +0800

    get request for loan amount based on loan id

--- client/loan-app/src/pages/Home2.js
+++ client/loan-app/src/pages/Home2.js
@@ -6,12 +6,15 @@ import Typography from "@mui/material/Typography";
 
 function Home2() {
   const [accBalance, setAccBalance] = useState();
+  const [loanAmount, setLoanAmount] = useState();
 
   useEffect(() => {
     async function getAccountBalance() {
       try {
-        const response = await api.getCustomerBalance(1);
-        setAccBalance(response.data[0]?.balance);
+        const accountData = await api.getCustomerBalance(1);
+        const loanData = await api.getLoanAmount(1);
+        setAccBalance(accountData.data[0]?.balance);
+        setLoanAmount(loanData.data[0]?.loan_amount);
       } catch (err) {
         console.log(err);
       }
@@ -47,6 +50,27 @@ function Home2() {
           {accBalance && (
             <Typography component="h5" variant="h5">
               {accBalance}
+            </Typography>
+          )}
+        </Box>
+        <Box
+          sx={{
+            width: 3 / 6,
+            borderRadius: "0.25rem",
+            boxShadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px",
+            display: "flex",
+            justifyContent: "space-evenly",
+            alignItems: "center",
+            mt: 4,
+            p: 4,
+          }}
+        >
+          <Typography component="h5" variant="h5">
+            Your Loan Amount:
+          </Typography>
+          {loanAmount && (
+            <Typography component="h5" variant="h5">
+              {loanAmount}
             </Typography>
           )}
         </Box>
