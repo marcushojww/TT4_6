f853ec4d8b7ae4175848fdc7982440eec1bf9c8a
Marcus Ho Jw <marcushojww@gmail.com>
2022-03-25 13:55:14 +0800

    navbar updates

--- client/loan-app/src/components/Layout.js
+++ client/loan-app/src/components/Layout.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate, NavLink } from "react-router-dom";
 import Box from "@mui/material/Box";
+import Avatar from "@mui/material/Avatar";
 import Typography from "@mui/material/Typography";
 
 function Layout({ children }) {
@@ -17,13 +18,26 @@ function Layout({ children }) {
           alignItems: "center",
         }}
       >
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+          <Typography
+            component="h5"
+            variant="h5"
+            sx={{ color: "white", fontWeight: "bold", mr: 2 }}
+          >
+            Welcome User!
+          </Typography>
+          <Avatar
+            alt="Travis Howard"
+            src="https://image.shutterstock.com/image-vector/young-man-avatar-character-260nw-661669825.jpg"
+          ></Avatar>
+        </Box>
         <NavLink
           to={"/"}
           style={({ isActive }) => ({
             textDecoration: "none",
             color: isActive ? "white" : "grey",
             fontWeight: isActive ? "bold" : "normal",
-            marginLeft: "100px",
+            marginLeft: "50px",
           })}
         >
           Home
