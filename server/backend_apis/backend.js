0e7edcb0ccf20f84081eb09ac8b3e5db3a5b6167
Jing Keat <ngjingkeat72@gmail.com>
2022-03-25 13:29:42 +0800

    minor changes in post request

--- server/backend_apis/backend.js
+++ server/backend_apis/backend.js
@@ -5,6 +5,20 @@ const database = require('./database');
 router = express.Router();
 // created a router object, so that we can write functions for the API
 
+// testing if apis work, calling all customer table details
+router.get("/customer/all", (request, response) => {
+
+    database.connection.query(`select * from customer`, // the SQL query
+    (errors, records) => { 
+        if (errors) {
+            console.log(errors);
+            response.status(500).send("An error occured in the backend");
+        } else {
+            response.status(200).send(records);
+        }
+    })
+
+})
 
 // get customerbalance by customer id
 router.get("/customerbalance/by-cid", (request,response) => {
@@ -48,13 +62,13 @@ router.post("/customerloan/add", (request,response) => {
     database.connection.query(
         `Insert into 
         customerloan (CustomerLoanId, CustomerId, LoanId)
-        values (${customerloan.customerloanid},${customerloan.customerid},'${customerloan.loanid}')`, //the SQL query
+        values (${customerLoan.customerloanid},${customerLoan.customerid},'${customerLoan.loanid}')`, //the SQL query
         (errors,records) => {
             if (errors) {
                 console.log(errors);
                 response.status(500).send("Some error occured at the backend");
             } else {
-                response.status(200).send("Created new account!");
+                response.status(200).send("Created new customer loan!");
             }
         }
     )
