913a91a751ea6b7e6b6fbb8931b854ba1505fd55
Marcus Ho Jw <marcushojww@gmail.com>
2022-03-25 11:21:04 +0800

    add api, components folder, and add UI for login page

--- client/loan-app/src/pages/Login.js
+++ client/loan-app/src/pages/Login.js
@@ -1,7 +1,75 @@
 import React from "react";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
 
 function Login() {
-  return <div>Login</div>;
+  return (
+    <Grid container sx={{ height: "100vh" }}>
+      <Grid
+        item
+        xs={false}
+        sm={4}
+        md={7}
+        sx={{
+          backgroundImage:
+            "url(https://assets.bwbx.io/images/users/iqjWHBFdfxIU/i7yNjkjkKGr8/v1/-1x-1.jpg)",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      ></Grid>
+      <Grid item xs={12} sm={8} md={5}>
+        <Box
+          sx={{
+            mt: 32,
+            mx: 4,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            height: "100%",
+          }}
+        >
+          <Typography component="h1" variant="h5">
+            Welcome to DBS Loan Management
+          </Typography>
+          <Box
+            component="form"
+            noValidate
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              width: "100%",
+              mt: 3,
+            }}
+          >
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              type="email"
+              autoComplete="email"
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+          </Box>
+        </Box>
+      </Grid>
+    </Grid>
+  );
 }
 
 export default Login;
