0926a3c3f50a95b85e923c0bacffe58e5d9b06df
Marcus Ho Jw <marcushojww@gmail.com>
2022-03-25 14:33:31 +0800

    Implement logout and add home 2

--- client/loan-app/src/pages/Home.js
+++ client/loan-app/src/pages/Home.js
@@ -1,49 +1,45 @@
 import React from "react";
-import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import Footer from "./Footer";
-
-
-function Home() {
-  return <div class="container-fluid">
-  <div class="row content">
-    <div class="col-sm-2">
-      <h2>Home</h2>
-    </div>
-    <div class="col-sm-10">
-      <div class="well">
-        <h4>Welcome!</h4>
-        <p>name</p>
-      </div>
-      <div class="row">
-        <div class="col-sm-4">
-          <div class="well">
-            <h4>Current Account Balance</h4>
-            <p>$ amount</p> 
-          </div>
-        </div>
-        <div class="col-sm-3">
-          <div class="well">
-            <h4>Current Loans</h4>
-            <p>$ amount</p> 
-          </div>
-        </div>
-        <div class="col-sm-3">
-          <div class="well">
-            <h4>Loan History</h4>
-            <p>$ amount</p> 
-          </div>
-          </div>
-          </div>
-          </div>
-  </div>
-  <Footer></Footer>
-  </div>
-;
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import Layout from "../components/Layout";
 
 function Home() {
-  return <Layout>Home</Layout>;
+  return (
+    <Layout>
+      <div class="container-fluid">
+        <div class="row content">
+          <div class="col-sm-2">
+            <h2>Home</h2>
+          </div>
+          <div class="col-sm-10">
+            <div class="well">
+              <h4>Welcome!</h4>
+              <p>name</p>
+            </div>
+            <div class="row">
+              <div class="col-sm-4">
+                <div class="well">
+                  <h4>Current Account Balance</h4>
+                  <p>$ amount</p>
+                </div>
+              </div>
+              <div class="col-sm-3">
+                <div class="well">
+                  <h4>Current Loans</h4>
+                  <p>$ amount</p>
+                </div>
+              </div>
+              <div class="col-sm-3">
+                <div class="well">
+                  <h4>Loan History</h4>
+                  <p>$ amount</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
 }
-
 
 export default Home;
