8ff90e615f396935d5959bb91745f9265ff3d71b
andreagomez1 <84144316+andreagomez1@users.noreply.github.com>
2022-03-25 12:42:29 +0800

    created home page layout

--- client/loan-app/src/pages/Home.js
+++ client/loan-app/src/pages/Home.js
@@ -1,7 +1,41 @@
 import React from "react";
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 function Home() {
-  return <div>Home</div>;
+  return <div class="container-fluid">
+  <div class="row content">
+    <div class="col-sm-2">
+      <h2>Home</h2>
+    </div>
+    <div class="col-sm-10">
+      <div class="well">
+        <h4>Welcome!</h4>
+        <p>name</p>
+      </div>
+      <div class="row">
+        <div class="col-sm-4">
+          <div class="well">
+            <h4>Current Account Balance</h4>
+            <p>$ amount</p> 
+          </div>
+        </div>
+        <div class="col-sm-3">
+          <div class="well">
+            <h4>Current Loans</h4>
+            <p>$ amount</p> 
+          </div>
+        </div>
+        <div class="col-sm-3">
+          <div class="well">
+            <h4>Loan History</h4>
+            <p>$ amount</p> 
+          </div>
+          </div>
+          </div>
+          </div>
+  </div>
+  </div>;
 }
+
 
 export default Home;
